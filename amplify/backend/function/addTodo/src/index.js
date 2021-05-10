f21c7abf6ae2d32f20192a71954363c7a45421aa
Lucas Marioni <lucas.marioni@geoactio.com>
2021-05-10 17:13:00 -0300

    add Layer + function

---
+++ amplify/backend/function/addTodo/src/index.js
@@ -0,0 +1,16 @@
+const {v4: uuid} = require(process.env.NODE_MODULES + 'uuid')
+
+exports.handler = async (event) => {
+    // TODO implement
+    const a = uuid()
+    const response = {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
