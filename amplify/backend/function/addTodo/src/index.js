8d7f933beabdf66c7a454014b871918db5e3042a
Lucas Marioni <lucas.marioni@geoactio.com>
2021-05-10 19:09:45 -0300

    deploy

--- amplify/backend/function/addTodo/src/index.js
+++ amplify/backend/function/addTodo/src/index.js
@@ -3,6 +3,8 @@ const {v4: uuid} = require(process.env.NODE_MODULES + 'uuid')
 exports.handler = async (event) => {
     // TODO implement
     const a = uuid()
+    console.log('a', a)
+    const b = uuid()
     const response = {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
